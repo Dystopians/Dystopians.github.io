8f26eb72628cce0b390f0aac8dbb05421884e2cd
Dystopians <karamazovaniki@gmail.com>
2026-05-30 13:27:18 -0700

    Localize Li Wenliang event framing

--- covid/app.js
+++ covid/app.js
@@ -3,7 +3,7 @@
 
   const STORAGE_KEY = "linjiang72-save-v2";
   const ASSET_PATH = "./assets/";
-  const ASSET_VERSION = "v29";
+  const ASSET_VERSION = "v30";
   const core = window.Linjiang72;
 
   let state = null;
